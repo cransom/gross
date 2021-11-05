de7e9d7d86b240c16f255661534b9047a933749f
Casey Ransom <casey@dailykos.com>
2021-11-04 23:07:25 -0400

    ignores

--- shell.nix
+++ shell.nix
@@ -3,7 +3,13 @@ in pkgs.mkShell {
   buildInputs = with pkgs; [
     ruby
     go
+    nodejs
+    perl
+    yarn
     python3Packages.virtualenv
     python3Packages.tkinter
+    libmemcached
+    memcached
+    pkg-config
   ];
 }
